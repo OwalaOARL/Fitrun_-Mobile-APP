90f27b2a927f8e661d2bfc9e190cc016026ffe59
OwalaOARL <112147607+OwalaOARL@users.noreply.github.com>
2025-01-05 11:32:27 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,9 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
 import React from "react";
-import Start from "./src/start"; // Import the Start component
+import Start from "./src/start";
+import Login from "./src/login";
+import SignIn from "./src/SignIn";
+import Home from "./src/Home"; // Import the Start component
 
 export default function App() {
   return <Start />; // Render the Start component
 }
+
 
